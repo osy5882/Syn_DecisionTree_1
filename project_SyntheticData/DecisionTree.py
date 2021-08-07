da8b696e2381e564f609863e30a3c9ddb4cb4d3c
osy5882 <osy5882@gmail.com>
2021-08-07 10:20:04 +0900

    recommit

---
+++ project_SyntheticData/DecisionTree.py
@@ -0,0 +1,224 @@
+import pandas as pd
+
+df = pd.read_csv("C:\data\AdultSalary_header.csv")
+df = df.iloc[:50, :]
+
+import math
+from collections import Counter, defaultdict
+from functools import partial
+
+
+class DecisionTree():
+    def __init__(self, df):
+        self.df = df
+
+    # label 제외하고 dict형태로 변환
+    def _get_data(self):
+        adult = self.df.iloc[:, :-1]
+        df_dict = adult.to_dict('record')
+        return df_dict
+
+    # label들을 list로 만듦.
+    def _labeling(self):
+        labels = self.df.iloc[:, -1]
+        return labels.to_list()
+
+    # 위의 두 return 값들을 zip함수 사용하여 원하는 데이터 형태인 inputs을 return
+    # 이제 Decisino Tree class에 적용할 수 있는 데이터 형태 만들어짐.
+    def comb_data(self):
+        data = self._get_data()
+        labels = self._labeling()
+        inputs = []
+        for pair in zip(data, labels):
+            inputs.append(pair)
+        return inputs
+
+        # 클래스에 속할 확률 입력하면 엔트로피 구하는 함수
+
+    def _entropy(self, class_probabilities):
+        return sum(-p * math.log(p, 2) for p in class_probabilities if p != 0)
+
+    # 각 레이블의 가중치를 구하는 메소드
+    # 해당 레이블 개수/전체 레이블 개수
+    def _class_probabilities(self, labels):
+        total_count = len(labels)
+        return [float(count) / float(total_count) for count in Counter(labels).values()]
+        ''' Counter(labels) = {0 : 3, 1 : 2} 이런식으로 나옴. 
+        return값은 0.6, 0.4 이런식으로 나옴.'''
+
+    # 노드의 엔트로피 계산하는 메소드
+    def _data_entropy(self, labeled_data):
+        labels = [label for _, label in labeled_data]
+        ''' _ 이게 있어야 True, False만 걸러져서 나옴. 없으면 전체 튜플이 나옴.'''
+        # for label in labeled_data:
+        #   print('label = {}'.format(label))
+        # print('labels = {}'.format(labels))
+
+        '''labels = [False, False, False, True, True] 이렇게 나옴.'''
+        probabilities = self._class_probabilities(labels)
+        return self._entropy(probabilities)
+
+    # 분할노드의 엔트로피를 정의하는 메소드
+    # SUM(자식노드(subset)의 엔트로피 * 가중치)가 분할노드(부모노드)의 엔트로피가 됨.
+    def _partition_entropy(self, subsets):
+        '''subsets는 inputs 데이터를 하나의 속성으로 구분해놓은 상태.'''
+        total_count = sum(len(subset) for subset in subsets)
+        ''' subset = [({'level': 'Senior', 'lang': 'Java', 'tweets': 'no', 'phd': 'no'}, False), ({'level': 'Senior', 'lang': 'Java', 'tweets': 'no', 'phd': 'yes'}, False), ({'level': 'Senior', 'lang': 'Python', 'tweets': 'no', 'phd': 'no'}, False), ({'level': 'Senior', 'lang': 'R', 'tweets': 'yes', 'phd': 'no'}, True), ({'level': 'Senior', 'lang': 'Python', 'tweets': 'yes', 'phd': 'yes'}, True)]
+            subset = [({'level': 'Mid', 'lang': 'Python', 'tweets': 'no', 'phd': 'no'}, True), ({'level': 'Mid', 'lang': 'R', 'tweets': 'yes', 'phd': 'yes'}, True), ({'level': 'Mid', 'lang': 'Python', 'tweets': 'no', 'phd': 'yes'}, True), ({'level': 'Mid', 'lang': 'Java', 'tweets': 'yes', 'phd': 'no'}, True)]
+            subset = [({'level': 'Junior', 'lang': 'Python', 'tweets': 'no', 'phd': 'no'}, True), ({'level': 'Junior', 'lang': 'R', 'tweets': 'yes', 'phd': 'no'}, True), ({'level': 'Junior', 'lang': 'R', 'tweets': 'yes', 'phd': 'yes'}, False), ({'level': 'Junior', 'lang': 'Python', 'tweets': 'yes', 'phd': 'no'}, True), ({'level': 'Junior', 'lang': 'Python', 'tweets': 'no', 'phd': 'yes'}, False)].'''
+        return sum(self._data_entropy(subset) * (len(subset) / total_count) for subset in subsets)
+
+    def _partition_by_cat(self, inputs, attribute):
+        groups = defaultdict(list)
+        # categorical_data = self.df[self.split()[0]].to_dict('record')
+        # 한번에 inputs를 받아서 카테고리 컬 데이터만 뽑은 다음에 groups를 만들기 근데 이렇게 하기 위해서는 전처리클래스를 상속받아야만 가능함
+        for input in inputs:
+            '''
+            ({'age': 39, 'workclass': ' State-gov', 'fnlwgt': 77516, 'education': ' Bachelors', 'edunum': 13, 'marital_status': ' Never-married', 'occupation': ' Adm-clerical', 'relationship': ' Not-in-family', 'race': ' White', 'sex': ' Male', 'capital_gain': 2174, 'capital_loss': 0, 'hours_per_works': 40, 'native_country': ' United-States'}, ' <=50K')
+            얘가 input '''
+            key = input[0][attribute]  # key는 workclass의 value들.
+            # print(key)   # 계속 돌다가 salary_class(레이블임) 까지 가면 에러 뜸.
+            groups[key].append(input)
+        return groups
+
+    def _partition_by_con(self, inputs, attribute):
+        Q1 = self.df[attribute].quantile(.25)  # 나중에 효율 따질 때 df 불러오는 거 다시 고려.
+        Q2 = self.df[attribute].quantile(.5)
+        Q3 = self.df[attribute].quantile(.75)
+        groups1 = defaultdict(list)  # q1 담을 곳
+        groups2 = defaultdict(list)  # q2 담을 곳
+        groups3 = defaultdict(list)  # q3 담을 곳
+        for input1 in inputs:
+            if input1[0][attribute] < Q1:
+                key = '%s_lower_Q1' % (attribute)
+                groups1[key].append(input1)
+            else:
+                key = '%s_upper_Q1' % (attribute)
+                groups1[key].append(input1)
+        for input2 in inputs:
+            if input2[0][attribute] < Q2:
+                key = '%s_lower_Q2' % (attribute)
+                groups2[key].append(input2)
+            else:
+                key = '%s_upper_Q2' % (attribute)
+                groups2[key].append(input2)
+        for input3 in inputs:
+            if input3[0][attribute] < Q3:
+                key = '%s_lower_Q3' % (attribute)
+                groups3[key].append(input3)
+            else:
+                key = '%s_upper_Q3' % (attribute)
+                groups3[key].append(input3)
+        return groups1, groups2, groups3
+
+        # 양적데이터와 카테고리컬 데이터 구분.
+
+    def _split(self):
+        df = self.df.iloc[:, :-1]
+        first = self.df.iloc[0, :]
+        attributes = self.df.columns
+        continuous = []
+        categorical = []
+        for i in range(
+                len(first) - 1):  # 마지막 column 을 빼기위해 range() 에서 -1해줌. 원래는 lable column을 빼줘야함. --> 나중에 재현데이터 할 때 손봐야함.
+            if str(first[i]).isdigit():
+                continuous.append(attributes[i])
+            else:
+                categorical.append(attributes[i])
+        return categorical, continuous
+
+    def _partition_entropy_by(self, inputs, attribute):
+        if attribute in self._split()[0]:  # 카테고리컬 데이터인 경우.
+            partitions = self._partition_by_cat(inputs, attribute)
+            return self._partition_entropy(partitions.values())
+
+        elif attribute in self._split()[1]:  # 양적 데이터인 경우.
+            partitions = self._partition_by_con(inputs, attribute)
+            t1 = self._partition_entropy(partitions[0].values())
+            t2 = self._partition_entropy(partitions[1].values())
+            t3 = self._partition_entropy(partitions[2].values())
+            return min(t1, t2, t3)
+
+    def build_tree(self, inputs, split_candidates=None):
+        if split_candidates is None:
+            split_candidates_cat = self._split()[0]  # 여기서 먼저 양적, 카테고리컬 속성 후보 나눔.
+            split_candidates_con = self._split()[1]
+            split_candidates = split_candidates_cat + split_candidates_con
+            # print(split_candidates)
+            # print(split_candidates_cat, split_candidates_con)
+
+        # 얜 지금 이분법 categorical 문제.
+        num_inputs = len(inputs)
+        num_class0 = len([label for _, label in inputs if label == ' <=50K'])
+        num_class1 = num_inputs - num_class0
+
+        if num_class0 == 0:
+            return ' >50K'
+        if num_class1 == 0:
+            return ' <=50K'
+
+        if not split_candidates:
+            if num_class0 >= num_class1:
+                return ' <=50K'
+            else:
+                return ' >50K'
+
+        best_attribute = min(split_candidates, key=partial(self._partition_entropy_by, inputs))  # 어려워ㅓㅓ
+        print(best_attribute)
+
+        if best_attribute in self._split()[0]:  # 카테고리컬일때
+            partitions = self._partition_by_cat(inputs, best_attribute)
+
+        elif best_attribute in self._split()[1]:  # 양적일때
+            partitions_tuple = self._partition_by_con(inputs, best_attribute)
+            partitions = defaultdict(list)
+            for i in range(3):
+                keys = partitions_tuple[i].keys()  # keys = [upper, lower]
+                for key in keys:
+                    partitions[key] = partitions_tuple[i][key]
+            # return partitions !!!!!!!
+        # print(partitions)
+
+        new_candidates = [a for a in split_candidates if a != best_attribute]
+        # print(new_candidates)
+
+        subtrees = {attribute_value: self.build_tree(subset, new_candidates) for attribute_value, subset in partitions.items()}
+
+        ''' best attribute 가 age이면 return 값이 group123이니까 그것들이 tuple로 묶여서 (group1, group2, group3) 이런식으로 나옴.
+        tuple은 items()가 안먹힘. 그 tuple에서 하나씩 뽑아서 groups하나의 형태로 만들면 됨.
+        '''
+
+        if num_class0 >= num_class1:
+            subtrees[None] = ' <=50K'
+        elif num_class0 < num_class1:
+            subtrees[None] = ' >50K'
+        return (best_attribute, subtrees)
+
+
+class Classify:
+
+    # def __init__(self, tree, input):
+    #     self.tree = tree
+    #     self.input = input
+
+    def classify_tree(self, tree, input):
+        if tree in [' <=50K', ' >50K']:  # 마지막에 leaf node 결정할 때 사용되는 코드
+            return tree
+
+        attribute, subtree_dict = tree  # 위에서 return 받은 (best_attribute, subtrees) 를 받는 것.
+
+        subtree_key = input.get(attribute)  # dictionary함수에서 key에 대응하는 value값을 받아옴.
+        if subtree_key not in subtree_dict:
+            subtree_key = None
+        # print(subtree_key, end='\n')
+
+        subtree = subtree_dict[subtree_key]
+        return self.classify_tree(subtree, input)
+
+
+test_dt = DecisionTree(df)
+inputs = test_dt.comb_data()
+tree = test_dt.build_tree(inputs)
+print(tree)
+
+
